613adb0adb43f3578005008f8bda049e9cdda15a
komalll07 <komalptl774@gmail.com>
2024-07-15 15:40:32 +0530

    Change in .jsx file

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -4,13 +4,14 @@ import React from 'react';
 const Button = ({ label, primary }) => {
   const style = {
     backgroundColor: primary ? 'blue' : 'red',
-    color: 'yellow',
+    color: 'pink',
     padding: '10px 20px',
     border: 'none',
     borderRadius: '5px',
+    backgroundColor:'yellow'
   };
 
-  return <button style={style}>{label} chromatic</button>;
+  return <button style={style}>{label} </button>;
 };
 
 export default Button;
